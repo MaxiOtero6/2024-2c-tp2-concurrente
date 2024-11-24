f98db7e70b295cb6f55b31b960c4d835cecfbe28
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2024-11-24 20:40:16 -0300

    feat: send and recv Send and receive data in driver connection

--- driver/src/concu_driver/driver_connection.rs
+++ driver/src/concu_driver/driver_connection.rs
@@ -1,10 +1,17 @@
 use std::{
-    os::unix::net::SocketAddr,
+    error::Error,
+    net::SocketAddr,
     sync::{Arc, Mutex},
 };
 
-use actix::{Actor, Addr, Context, Message};
-use tokio::{io::WriteHalf, net::TcpStream};
+use actix::{
+    dev::ContextFutureSpawner, fut::wrap_future, Actor, ActorContext, Addr, AsyncContext, Context,
+    Handler, Message, StreamHandler,
+};
+use tokio::{
+    io::{AsyncWriteExt, WriteHalf},
+    net::TcpStream,
+};
 
 use super::central_driver::CentralDriver;
 
@@ -17,6 +24,39 @@ pub struct DriverConnection {
     driver_addr: Option<SocketAddr>,
 }
 
+impl DriverConnection {
+    pub fn new(
+        self_driver_addr: Addr<CentralDriver>,
+        wstream: Arc<Mutex<WriteHalf<TcpStream>>>,
+        driver_addr: Option<SocketAddr>,
+    ) -> Self {
+        DriverConnection {
+            central_driver: self_driver_addr,
+            driver_write_stream: wstream,
+            driver_addr,
+        }
+    }
+}
+
+impl StreamHandler<Result<String, std::io::Error>> for DriverConnection {
+    fn handle(&mut self, msg: Result<String, std::io::Error>, ctx: &mut Self::Context) {
+        if let Ok(data) = msg {
+            log::debug!("recv {}", data);
+
+            let _ = ctx
+                .address()
+                .try_send(RecvAll { data })
+                .inspect_err(|e| log::error!("{}", e.to_string()));
+        }
+    }
+
+    fn finished(&mut self, ctx: &mut Self::Context) {
+        // Election
+
+        ctx.stop();
+    }
+}
+
 impl Actor for DriverConnection {
     type Context = Context<Self>;
 }
@@ -26,4 +66,46 @@ impl Actor for DriverConnection {
 struct LeaderStatus {
     driver_ids: Vec<u32>,
     msg_type: String,
+}
+
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct SendAll {
+    pub data: String,
+}
+
+impl Handler<SendAll> for DriverConnection {
+    type Result = ();
+
+    fn handle(&mut self, msg: SendAll, ctx: &mut Context<Self>) -> Self::Result {
+        let message = msg.data + "\n";
+
+        let w = self.driver_write_stream.clone();
+        wrap_future::<_, Self>(async move {
+            if let Ok(mut writer) = w.lock() {
+                let _ = writer
+                    .write_all(message.as_bytes())
+                    .await
+                    .inspect_err(|e| log::error!("{}", e.to_string()));
+                let _ = writer
+                    .flush()
+                    .await
+                    .inspect_err(|e| log::error!("{}", e.to_string()))
+                    .inspect(|_| log::debug!("sent {}", message));
+            }
+        })
+        .spawn(ctx);
+    }
+}
+
+#[derive(Message)]
+#[rtype(result = "()")]
+pub struct RecvAll {
+    pub data: String,
+}
+
+impl Handler<RecvAll> for DriverConnection {
+    type Result = ();
+
+    fn handle(&mut self, msg: RecvAll, _ctx: &mut Context<Self>) -> Self::Result {}
 }
