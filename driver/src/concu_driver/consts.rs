c7d1f9e9fe417d872fae5c793bf5e03e7b01a287
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2024-11-24 20:35:02 -0300

    feat: more consts

--- driver/src/concu_driver/consts.rs
+++ driver/src/concu_driver/consts.rs
@@ -1,4 +1,9 @@
+use log::LevelFilter;
+
 pub const LEADER_PORT: u32 = 8080;
 pub const HOST: &str = "0.0.0.0";
 pub const MIN_DRIVER_PORT: u32 = 8081;
 pub const MAX_DRIVER_PORT: u32 = 8101;
+pub const MAX_DISTANCE: u32 = 10;
+pub const PAYMENT_PORT: u32 = 3000;
+pub const LOG_LEVEL: LevelFilter = LevelFilter::Debug;
