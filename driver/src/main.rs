6e8d6809ef5cfeaf4efb7f6acc2a5463e48a3043
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2024-11-24 20:18:44 -0300

    feat: setup logger

--- driver/src/main.rs
+++ driver/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use driver::concu_driver::consts::LOG_LEVEL;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::builder()
+        .filter_level(LOG_LEVEL)
+        .init();
+
+    driver::run()
 }
