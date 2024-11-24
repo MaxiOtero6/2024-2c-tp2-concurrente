fc351be807524f786615f872b31c03059d5d3621
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2024-11-23 23:20:58 -0300

    feat: position struct

---
+++ driver/src/concu_driver/position.rs
@@ -0,0 +1,27 @@
+use rand::Rng;
+
+pub struct Position {
+    x: u32,
+    y: u32,
+}
+
+impl Position {
+    pub fn new(x: u32, y: u32) -> Self {
+        Self { x, y }
+    }
+
+    pub fn random() -> Self {
+        let mut rng = rand::thread_rng();
+        Self {
+            x: rng.gen_range(0..100),
+            y: rng.gen_range(0..100),
+        }
+    }
+
+    pub fn distance_to(self, p: Position) -> u32 {
+        let x: u32 = (p.x as i32 - self.x as i32).abs() as u32;
+        let y: u32 = (p.y as i32 - self.y as i32).abs() as u32;
+
+        x + y
+    }
+}
