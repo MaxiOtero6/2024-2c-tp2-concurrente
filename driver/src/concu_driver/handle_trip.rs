6ace3d19ec6bbccd23f577c453ca8772e0db5bbe
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2024-11-24 20:37:43 -0300

    feat: TripHandler constructor

--- driver/src/concu_driver/handle_trip.rs
+++ driver/src/concu_driver/handle_trip.rs
@@ -1,20 +1,34 @@
 use actix::{Actor, Addr, Context, Message};
 
-use super::central_driver::CentralDriver;
+use super::{
+    central_driver::{self, CentralDriver},
+    position::Position,
+};
 
 pub struct TripHandler {
     // Direccion del actor CentralDriver
     central_driver: Addr<CentralDriver>,
+    // Posicion actual del driver
+    current_location: Position,
 }
 
 impl Actor for TripHandler {
     type Context = Context<Self>;
 }
 
+impl TripHandler {
+    pub fn new(central_driver: Addr<CentralDriver>) -> Self {
+        Self {
+            central_driver,
+            current_location: Position::random(),
+        }
+    }
+}
+
 #[derive(Message)]
 #[rtype(result = String)]
 struct TripStart {
     passenger_id: u32,
-    passenger_location: (u32, u32),
-    destination: (u32, u32),
+    passenger_location: Position,
+    destination: Position,
 }
