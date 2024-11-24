718892598cb63ad95525401f8a171a1c2ddcb2ed
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2024-11-24 20:31:09 -0300

    feat: connect with Payment Service

--- driver/src/concu_driver/payment_connection.rs
+++ driver/src/concu_driver/payment_connection.rs
@@ -1,25 +1,58 @@
-use std::{
+use std::sync::{Arc, Mutex};
+
+use actix::{Actor, Addr, AsyncContext, Context, Message, StreamHandler};
+use tokio::{
+    io::{split, AsyncBufReadExt, BufReader, WriteHalf},
     net::TcpStream,
-    os::unix::net::SocketAddr,
-    sync::{Arc, Mutex},
 };
+use tokio_stream::wrappers::LinesStream;
 
-use actix::{Actor, Addr, Context, Message};
-use tokio::io::WriteHalf;
-
-use super::central_driver::CentralDriver;
+use super::{
+    central_driver::CentralDriver,
+    consts::{HOST, PAYMENT_PORT},
+};
 
 pub struct PaymentConnection {
     // Direccion del actor CentralDriver
     central_driver: Addr<CentralDriver>,
     // Stream para enviar al payment
     payment_write_stream: Arc<Mutex<WriteHalf<TcpStream>>>,
-    // Direccion del stream del payment
-    payment_addr: Option<SocketAddr>,
 }
 
 impl Actor for PaymentConnection {
     type Context = Context<Self>;
+}
+
+impl PaymentConnection {
+    fn new(central_driver: Addr<CentralDriver>, write_stream: WriteHalf<TcpStream>) -> Self {
+        Self {
+            central_driver,
+            payment_write_stream: Arc::new(Mutex::new(write_stream)),
+        }
+    }
+
+    pub async fn connect(
+        central_driver: Addr<CentralDriver>,
+    ) -> Result<Addr<PaymentConnection>, String> {
+        log::debug!("Trying to connect with payments service");
+
+        let addr = format!("{}:{}", HOST, PAYMENT_PORT);
+
+        let socket = TcpStream::connect(addr.clone())
+            .await
+            .map_err(|e| e.to_string())?;
+
+        let (r, w) = split(socket);
+
+        Ok(PaymentConnection::create(|ctx| {
+            ctx.add_stream(LinesStream::new(BufReader::new(r).lines()));
+            PaymentConnection::new(central_driver, w)
+        }))
+    }
+}
+
+impl StreamHandler<Result<String, std::io::Error>> for PaymentConnection {
+    fn handle(&mut self, msg: Result<String, std::io::Error>, ctx: &mut Self::Context) {}
 }
 
 #[derive(Message)]
