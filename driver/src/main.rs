3e6111fd7d7356429a22da5a232a3d951d508fc1
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2024-11-25 18:20:49 -0300

    refactor: log format

--- driver/src/main.rs
+++ driver/src/main.rs
@@ -5,6 +5,10 @@ use driver::concu_driver::consts::LOG_LEVEL;
 fn main() -> Result<(), Box<dyn Error>> {
     env_logger::builder()
         .filter_level(LOG_LEVEL)
+        .format_target(false)
+        .format_module_path(false)
+        // .format_timestamp_micros()
+        .format_timestamp(None)
         .init();
 
     driver::run()
