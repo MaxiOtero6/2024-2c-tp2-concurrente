a457b6864ae28aafdfa3e179cf93d17e7c85e156
Maximiliano Otero Silvera <maximilianotero7@gmail.com>
2024-11-25 21:36:39 -0300

    feat: init payment project

---
+++ payment/src/concu_payment/payment.rs
@@ -0,0 +1,5 @@
+use std::error::Error;
+
+pub fn handle_payments() -> Result<(), Box<dyn Error>> {
+
+}
--- payment/src/main.rs
+++ payment/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+use payment::concu_payment::consts::LOG_LEVEL;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::builder()
+        .filter_level(LOG_LEVEL)
+        .format_target(false)
+        .format_module_path(false)
+        // .format_timestamp_micros()
+        .format_timestamp(None)
+        .init();
+
+    payment::run()
 }
