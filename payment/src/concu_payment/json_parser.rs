7f889340b3261cfdb004b8a1f8fc4e9dadaf913e
Tinchocw <mcwikla@fi.uba.ar>
2024-11-27 23:43:19 -0300

    feat: Handle parsing error

--- payment/src/concu_payment/json_parser.rs
+++ payment/src/concu_payment/json_parser.rs
@@ -1,12 +1,12 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Deserialize)]
 pub enum PaymentMessages {
     AuthPayment { passenger_id: u32 },
     CollectPayment { driver_id: u32, passenger_id: u32 },
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize)]
 pub enum PaymentResponses {
     AuthPayment { passenger_id: u32, response: bool },
     CollectPayment { passenger_id: u32, response: bool },
--- payment/src/lib.rs
+++ payment/src/lib.rs
@@ -5,15 +5,5 @@ use concu_payment::payment::handle_payments;
 pub mod concu_payment;
 
 pub fn run() -> Result<(), Box<dyn Error>> {
-    let argv: Vec<String> = std::env::args().collect();
-
-    // if argv.len() != 2 {
-    //     return Err("Wrong args, expected: <program> <driver_id>".into());
-    // }
-
-    // let driver_id = argv[1]
-    //     .parse::<u32>()
-    //     .expect("Wrong driver_id, value must be parseable to u32");
-
     handle_payments()
 }
